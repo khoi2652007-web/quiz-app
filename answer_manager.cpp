21b19d24d399c384623094380567f7e9399f3263
hoanggiahuy19 <huyhg899721@ut.edu.vn>
2026-07-01 00:46:49 +0700

    Huy hoan thanh code quan ly cau tra loi.

--- answer_manager.cpp
+++ answer_manager.cpp
@@ -1,1 +1,47 @@
- 
+#include "answer_manager.h"
+
+AnswerManager::AnswerManager() {}
+
+void AnswerManager::recordAnswer(int questionNumber, char answer) {
+    studentAnswers[questionNumber] = answer;
+}
+
+char AnswerManager::getAnswer(int questionNumber) const {
+    auto it = studentAnswers.find(questionNumber);
+    if (it != studentAnswers.end()) {
+        return it->second;
+    }
+    return ' ';
+}
+
+std::vector<int> AnswerManager::getSkippedQuestions(int totalQuestions) const {
+    std::vector<int> skipped;
+    for (int i = 1; i <= totalQuestions; ++i) {
+        if (studentAnswers.find(i) == studentAnswers.end()) {
+            skipped.push_back(i);
+        }
+    }
+    return skipped;
+}
+
+bool AnswerManager::showWarningIfSkipped(int totalQuestions) const {
+    std::vector<int> skipped = getSkippedQuestions(totalQuestions);
+
+    if (skipped.empty()) {
+        return false;
+    }
+
+    std::cout << "\n[CANH BAO] Ban chua tra loi cac cau hoi sau: ";
+    for (size_t i = 0; i < skipped.size(); ++i) {
+        std::cout << skipped[i];
+        if (i < skipped.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "\nBan co chac chan muon nop bai khong? (Y/N): ";
+    return true;
+}
+
+std::map<int, char> AnswerManager::getAllAnswers() const {
+    return studentAnswers;
+}
--- answer_manager.h
+++ answer_manager.h
@@ -1,1 +1,22 @@
- 
+#ifndef ANSWER_MANAGER_H
+#define ANSWER_MANAGER_H
+
+#include <map>
+#include <vector>
+#include <iostream>
+
+class AnswerManager {
+private:
+    std::map<int, char> studentAnswers;
+
+public:
+    AnswerManager();
+
+    void recordAnswer(int questionNumber, char answer);
+    char getAnswer(int questionNumber) const;
+    std::vector<int> getSkippedQuestions(int totalQuestions) const;
+    bool showWarningIfSkipped(int totalQuestions) const;
+    std::map<int, char> getAllAnswers() const;
+};
+
+#endif // ANSWER_MANAGER_H
