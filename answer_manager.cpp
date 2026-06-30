fc4e3bff455e5fd4d72587e72388a72d0ac73f67
hoanggiahuy19 <huyhg899721@ut.edu.vn>
2026-07-01 01:42:03 +0700

    Huy cap nhat code theo review cua Khoi

--- answer_manager.cpp
+++ answer_manager.cpp
@@ -1,21 +1,13 @@
 #include "answer_manager.h"
 
-AnswerManager::AnswerManager() {}
+using namespace std;
 
-void AnswerManager::recordAnswer(int questionNumber, char answer) {
-    studentAnswers[questionNumber] = answer;
+void AnswerManager::recordAnswer(int questionIndex, char answer) {
+    studentAnswers[questionIndex] = answer;
 }
 
-char AnswerManager::getAnswer(int questionNumber) const {
-    auto it = studentAnswers.find(questionNumber);
-    if (it != studentAnswers.end()) {
-        return it->second;
-    }
-    return ' ';
-}
-
-std::vector<int> AnswerManager::getSkippedQuestions(int totalQuestions) const {
-    std::vector<int> skipped;
+vector<int> AnswerManager::getSkippedQuestions(int totalQuestions) const {
+    vector<int> skipped;
     for (int i = 1; i <= totalQuestions; ++i) {
         if (studentAnswers.find(i) == studentAnswers.end()) {
             skipped.push_back(i);
@@ -25,23 +17,23 @@ std::vector<int> AnswerManager::getSkippedQuestions(int totalQuestions) const {
 }
 
 bool AnswerManager::showWarningIfSkipped(int totalQuestions) const {
-    std::vector<int> skipped = getSkippedQuestions(totalQuestions);
+    vector<int> skipped = getSkippedQuestions(totalQuestions);
 
     if (skipped.empty()) {
         return false;
     }
 
-    std::cout << "\n[CANH BAO] Ban chua tra loi cac cau hoi sau: ";
+    cout << "\n[CANH BAO] Ban chua tra loi cac cau hoi sau: ";
     for (size_t i = 0; i < skipped.size(); ++i) {
-        std::cout << skipped[i];
+        cout << skipped[i];
         if (i < skipped.size() - 1) {
-            std::cout << ", ";
+            cout << ", ";
         }
     }
-    std::cout << "\nBan co chac chan muon nop bai khong? (Y/N): ";
+    cout << "\nBan co chac chan muon nop bai khong? (Y/N): ";
     return true;
 }
 
-std::map<int, char> AnswerManager::getAllAnswers() const {
+map<int, char> AnswerManager::getAllAnswers() const {
     return studentAnswers;
 }
--- answer_manager.h
+++ answer_manager.h
@@ -1,22 +1,28 @@
-#ifndef ANSWER_MANAGER_H
-#define ANSWER_MANAGER_H
-
-#include <map>
-#include <vector>
+#pragma once
 #include <iostream>
+#include <vector>
+#include <map>
 
-class AnswerManager {
-private:
-    std::map<int, char> studentAnswers;
+using namespace std;
 
+// Lớp cơ sở để thể hiện tính kế thừa theo yêu cầu OOP của Khôi
+class BaseAnswerManager {
 public:
-    AnswerManager();
+    virtual ~BaseAnswerManager() {}
 
-    void recordAnswer(int questionNumber, char answer);
-    char getAnswer(int questionNumber) const;
-    std::vector<int> getSkippedQuestions(int totalQuestions) const;
-    bool showWarningIfSkipped(int totalQuestions) const;
-    std::map<int, char> getAllAnswers() const;
+    // Các hàm ảo sẽ được lớp con ghi đè
+    virtual void recordAnswer(int questionIndex, char answer) = 0;
+    virtual bool showWarningIfSkipped(int totalQuestions) const = 0;
 };
 
-#endif // ANSWER_MANAGER_H
+// Lớp AnswerManager kế thừa từ BaseAnswerManager
+class AnswerManager : public BaseAnswerManager {
+private:
+    map<int, char> studentAnswers;
+
+public:
+    void recordAnswer(int questionIndex, char answer) override;
+    vector<int> getSkippedQuestions(int totalQuestions) const;
+    bool showWarningIfSkipped(int totalQuestions) const override;
+    map<int, char> getAllAnswers() const;
+};
