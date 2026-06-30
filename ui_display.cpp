d1a6440d36f6fbf60f7ae1d5659735e70383be3c
hanld543242-alt <hanld543242@ut.edu.vn>
2026-06-30 21:08:10 +0700

    Add files via upload

--- ui_display.cpp
+++ ui_display.cpp
@@ -1,1 +1,43 @@
- 
+#include "ui_display.h"
+#include <iostream>
+using namespace std;
+
+Candidate::Candidate(string t, string m, string l) {
+    ten = t;
+    mssv = m;
+    lop = l;
+}
+
+string Candidate::getTen() const {
+    return ten;
+}
+
+string Candidate::getMSSV() const {
+    return mssv;
+}
+
+string Candidate::getLop() const {
+    return lop;
+}
+
+void UIDisplay::hienThiThongTinThiSinh(const Candidate& c) {
+    cout << "Ten: " << c.getTen() << endl;
+    cout << "MSSV: " << c.getMSSV() << endl;
+    cout << "Lop: " << c.getLop() << endl;
+}
+
+void UIDisplay::hienThiTongSoCauHoi(int tongSoCau) {
+    cout << "Tong so cau hoi: " << tongSoCau << endl;
+}
+
+bool UIDisplay::xacNhanNopBai() {
+    int luaChon;
+
+    cout << "Ban co chac muon nop bai?" << endl;
+    cout << "1. Co" << endl;
+    cout << "2. Khong" << endl;
+
+    cin >> luaChon;
+
+    return luaChon == 1;
+}
--- ui_display.h
+++ ui_display.h
@@ -1,1 +1,24 @@
- 
+#pragma once
+#include <string>
+using namespace std;
+
+class Candidate {
+private:
+    string ten;
+    string mssv;
+    string lop;
+
+public:
+    Candidate(string t, string m, string l);
+
+    string getTen() const;
+    string getMSSV() const;
+    string getLop() const;
+};
+
+class UIDisplay {
+public:
+    void hienThiThongTinThiSinh(const Candidate& c);
+    void hienThiTongSoCauHoi(int tongSoCau);
+    bool xacNhanNopBai();
+};
